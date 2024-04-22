0b97f5e33b9f6458bb515298d19fb9b4f95b97be
luminitapopescu <popesculumi@yahoo.com>
2024-04-22 18:43:11 +0300

    adaugare JDBC la temaLab3

--- Lab3/src/temaLab3/utils/Constants.java
+++ Lab3/src/temaLab3/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String PROFESSOR = "profesor";
 
     public static final String JDBC_DRIVER = "jdbc:mysql://localhost/proiectpao";
-    public static final String JDBC_PWD = "Stefan1lp@";
+    public static final String JDBC_PWD = "pwd";
     public static final String JDBC_USER = "root";
 
     public static final String AUDIT_FILE = "audit.csv";
